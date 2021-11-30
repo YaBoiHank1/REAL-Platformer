b83ed86e815bbf02074077baf4473f149d585943
Henry Owen <hnryowen1@gmail.com>
2021-11-30 10:03:18 -0800

    Player Sprites and animations
    Idle and run animations

--- New Unity Project/Assets/Scripts/PlatformerMovementWithFeet.cs
+++ New Unity Project/Assets/Scripts/PlatformerMovementWithFeet.cs
@@ -7,21 +7,45 @@ public class PlatformerMovementWithFeet : MonoBehaviour {
 	public float moveSpeed = 1.0f;
 	public float jumpSpeed = 1.0f;
 	private bool grounded = false;
+    private Animator myAnimator;
+    private SpriteRenderer myRenderer;
 
-	void Start () {
-
+	void Start ()
+    {
+        myAnimator = GetComponent<Animator>();
+        myRenderer = GetComponent<SpriteRenderer>();
+        myAnimator.SetBool("Moving", false);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		var moveX = Input.GetAxis ("Horizontal");
-		var velocity = gameObject.GetComponent<Rigidbody2D> ().velocity;
+		var velocity = gameObject.GetComponent<Rigidbody2D>().velocity;
 		velocity.x = moveX * moveSpeed;
 		gameObject.GetComponent<Rigidbody2D> ().velocity = velocity;
-		if (Input.GetButtonDown ("Jump") && grounded) {
+        if (velocity.x > 0 || velocity.x < 0)
+        {
+            myAnimator.SetBool("Moving", true);
+            myAnimator.SetBool("Idle", false);
+        }
+        else if (velocity.x == 0)
+        {
+            myAnimator.SetBool("Moving", false);
+            myAnimator.SetBool("Idle", true);
+        }
+		if (Input.GetButtonDown ("Jump") && grounded)
+        {
 			gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, 100 * jumpSpeed));
 		} 
-	}
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            myRenderer.flipX = false;
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            myRenderer.flipX = true;
+        }
+    }
 
 	public void Grounded(){
 		grounded = true;
