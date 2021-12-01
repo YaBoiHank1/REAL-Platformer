94c804f561496f6ea31d2ba688f9c1cad10a2fb0
DjHawks24 <22.dean.chilton@ksd.org>
2021-12-01 08:48:23 -0800

    Revert "Pause Menu up and working and some test things"
    This reverts commit 53794a6dd7e0bff36e4b0b412dbf134d7f471751.

--- New Unity Project/Assets/Scripts/PauseMenu.cs
+++ New Unity Project/Assets/Scripts/PauseMenu.cs
@@ -1,38 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour {
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-		if (Input.GetKeyDown (KeyCode.Escape) && Time.timeScale == 1) {
-			Time.timeScale = 0;
-			GetComponent<Canvas> ().enabled = true;
-		}else if(Input.GetKeyDown (KeyCode.Escape) && Time.timeScale == 0){
-			Resume();
-		}
-
-	}
-
-	public void Resume(){
-		Time.timeScale = 1;
-		GetComponent<Canvas> ().enabled = false;
-	}
-
-	public void ExitGame(){
-		Application.Quit ();
-	}
-
-	public void LoadMainMenu(){
-		Time.timeScale = 1;
-		SceneManager.LoadScene ("MainMenu");
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
