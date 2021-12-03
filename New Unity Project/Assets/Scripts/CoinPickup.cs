b840c90b7f25d025142adce254d705605c67b115
DjHawks24 <22.dean.chilton@ksd.org>
2021-12-03 09:24:47 -0800

    Updating test scene

--- New Unity Project/Assets/Scripts/CoinPickup.cs
+++ New Unity Project/Assets/Scripts/CoinPickup.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
-    [SerializeField] AudioClip CoinPickupSFX;
-    [SerializeField] int CoinValue = 100;
+    //[SerializeField] AudioClip CoinPickupSFX;
+    [SerializeField] int CoinValue = 1;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         //FindObjectOfType<GameSession>().AddToScore(CoinValue);
-        AudioSource.PlayClipAtPoint(CoinPickupSFX, Camera.main.transform.position);
+        //AudioSource.PlayClipAtPoint(CoinPickupSFX, Camera.main.transform.position);
         Destroy(gameObject);
 
     }
