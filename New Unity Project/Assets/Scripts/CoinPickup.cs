d3bdedec8bcdeb91e1219f226e7ffffd47443813
Dean Chilton <22.dean.chilton@ksd.org>
2021-12-08 10:02:05 -0800

    updating things

---
+++ New Unity Project/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    [SerializeField] int CoinValue = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       FindObjectOfType<GameSession>().AddToScore(CoinValue);
+       Destroy(gameObject);       
+    }
+}
