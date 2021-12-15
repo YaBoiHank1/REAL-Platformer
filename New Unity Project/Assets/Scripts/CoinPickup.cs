c92f076900a65fe2403b1e0f2d909f4281e34e41
Henry Owen <hnryowen1@gmail.com>
2021-12-15 08:21:49 -0800

    Dialogue in level 1

--- New Unity Project/Assets/Scripts/CoinPickup.cs
+++ New Unity Project/Assets/Scripts/CoinPickup.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] int CoinValue = 1;
-
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
-       FindObjectOfType<GameSession>().AddToScore(CoinValue);
+        var gameSession = FindObjectOfType<GameSession>() as GameSession;
+       gameSession.AddToScore(CoinValue);
        Destroy(gameObject);       
     }
 }
