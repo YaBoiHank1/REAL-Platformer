612c078f2ad84ddaff7e572de1441d8f341186d4
Henry Owen <hnryowen1@gmail.com>
2021-12-15 08:26:07 -0800

    Audio to coin pickup

--- New Unity Project/Assets/Scripts/CoinPickup.cs
+++ New Unity Project/Assets/Scripts/CoinPickup.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] int CoinValue = 1;
+    [SerializeField] AudioClip fleshFX;
     
     private void OnTriggerEnter2D(Collider2D other)
     {
         var gameSession = FindObjectOfType<GameSession>() as GameSession;
        gameSession.AddToScore(CoinValue);
+        AudioSource.PlayClipAtPoint(fleshFX, Camera.main.transform.position);
        Destroy(gameObject);       
     }
 }
